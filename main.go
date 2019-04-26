874c0d3f22466e0c906daaa559fc884fb4aa9f58
Paul Stuart <pauleyphonic@gmail.com>
2019-04-26 10:07:06 -0700

    move like with like, better formatting for kaboom

--- main.go
+++ main.go
@@ -10,13 +10,13 @@ import (
 	"strings"
 )
 
-type marker byte
-
 const (
 	clear  marker = iota
-	opaque marker = iota + 10
+	opaque marker = iota + 10 // +10 so we can use 1-9 as bomb markers
 	bomb
 )
+
+type marker byte
 
 func (m marker) String() string {
 	switch m {
@@ -35,12 +35,107 @@ func (m marker) String() string {
 
 type grid [][]marker
 
+func (g grid) line() {
+	fmt.Println("    " + strings.Repeat("----", len(g)))
+}
+
+func (g grid) show() {
+	// Header
+	fmt.Print("    ")
+	for x := range g {
+		fmt.Printf(" %2d ", x+1)
+	}
+	fmt.Println()
+	g.line()
+
+	// the matrix
+	for y, row := range g {
+		fmt.Printf(" %2d |", y+1)
+		for _, col := range row {
+			fmt.Printf(" %s |", col)
+		}
+		fmt.Println()
+		g.line()
+	}
+}
+
+func (g grid) valid(x, y int) bool {
+	return x >= 0 && x < len(g) && y >= 0 && y < len(g)
+}
+
+func (g grid) bombCheck(x, y int) int {
+	if g.valid(x, y) && (g[y][x] == bomb) {
+		return 1
+	}
+	return 0
+}
+
+func (g grid) nearby(x, y int) int {
+	// skip if the square is a bomb itself
+	if g.bombCheck(x, y) == 1 {
+		return 0
+	}
+
+	// clockwise evaluation makes it clear all neighbors are checked
+
+	count := 0
+	count += g.bombCheck(x, y-1)   // top
+	count += g.bombCheck(x+1, y-1) // top-right
+	count += g.bombCheck(x+1, y)   // right
+	count += g.bombCheck(x+1, y+1) // bottom-right
+	count += g.bombCheck(x, y+1)   // bottom
+	count += g.bombCheck(x-1, y+1) // bottom-left
+	count += g.bombCheck(x-1, y)   // left
+	count += g.bombCheck(x-1, y-1) // top-left
+	return count
+}
+
+func newGrid(size int) grid {
+	a := make(grid, size)
+	for i := range a {
+		a[i] = make([]marker, size)
+	}
+	return a
+}
+
+func populate(size, count int, g grid) grid {
+	for count > 0 {
+		x := rand.Intn(size)
+		y := rand.Intn(size)
+		if g[y][x] != bomb {
+			g[y][x] = bomb
+			count--
+		}
+	}
+	// mark counts for cells neighboring a bomb
+	for y := range g {
+		for x := range g[y] {
+			if count := g.nearby(x, y); count > 0 {
+				g[y][x] = marker(count)
+			}
+		}
+	}
+	return g
+}
+
+// make an fresh 'opaque' board
+func wipe(g grid) grid {
+	for _, row := range g {
+		for x := range row {
+			row[x] = opaque
+		}
+	}
+	return g
+}
+
+// Board represents the minesweeper game
 type Board struct {
 	Shown     grid
 	Mines     grid
 	Remaining int
 }
 
+// Prompt gets the next move of the game
 func (b *Board) Prompt() (bool, int, int) {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
@@ -84,125 +179,7 @@ func (b *Board) Prompt() (bool, int, int) {
 	panic("never here")
 }
 
-func (g grid) line() {
-	fmt.Println("    " + strings.Repeat("----", len(g)))
-}
-
-func (g grid) show() {
-	// Header
-	fmt.Print("    ")
-	for x := range g {
-		fmt.Printf(" %2d ", x+1)
-	}
-	fmt.Println()
-	g.line()
-	// the matrix
-	for y, row := range g {
-		fmt.Printf(" %2d |", y+1)
-		for _, col := range row {
-			fmt.Printf(" %s |", col)
-		}
-		fmt.Println()
-		g.line()
-	}
-}
-
-func newGrid(size int) grid {
-	a := make(grid, size)
-	for i := range a {
-		a[i] = make([]marker, size)
-	}
-	return a
-}
-
-func populate(size, count int, g grid) grid {
-	for count > 0 {
-		x := rand.Intn(size)
-		y := rand.Intn(size)
-		if g[y][x] != bomb {
-			g[y][x] = bomb
-			count--
-		}
-	}
-	// mark counts for cells neighboring a bomb
-	for y := range g {
-		for x := range g[y] {
-			if count := g.nearby(x, y); count > 0 {
-				g[y][x] = marker(count)
-			}
-		}
-	}
-	return g
-}
-
-// make an fresh 'opaque' board
-func wipe(g grid) grid {
-	for _, row := range g {
-		for x := range row {
-			row[x] = opaque
-		}
-	}
-	return g
-}
-
-func NewBoard(size, count int) *Board {
-	return &Board{
-		Mines:     populate(size, count, newGrid(size)),
-		Shown:     wipe(newGrid(size)),
-		Remaining: count,
-	}
-}
-
-func (g grid) valid(x, y int) bool {
-	return x >= 0 && x < len(g) && y >= 0 && y < len(g)
-}
-
-func (g grid) bombCheck(x, y int) int {
-	if g.valid(x, y) && (g[y][x] == bomb) {
-		return 1
-	}
-	return 0
-}
-
-func (g grid) nearby(x, y int) int {
-	// skip if the square is a bomb itself
-	if g.bombCheck(x, y) == 1 {
-		return 0
-	}
-
-	// clockwise evaluation makes it clear all neighbors are checked
-
-	count := 0
-	count += g.bombCheck(x, y-1)   // top
-	count += g.bombCheck(x+1, y-1) // top-right
-	count += g.bombCheck(x+1, y)   // right
-	count += g.bombCheck(x+1, y+1) // bottom-right
-	count += g.bombCheck(x, y+1)   // bottom
-	count += g.bombCheck(x-1, y+1) // bottom-left
-	count += g.bombCheck(x-1, y)   // left
-	count += g.bombCheck(x-1, y-1) // top-left
-	return count
-}
-
-func main() {
-	size := 10
-	count := 5
-
-	flag.IntVar(&size, "size", size, "grid size")
-	flag.IntVar(&count, "count", count, "number of mines")
-	flag.Parse()
-
-	// make sure input was valid (was passing args w/o flags and ignoring it
-	if len(flag.Args()) > 0 {
-		fmt.Println("unknown args:", flag.Args())
-		os.Exit(1)
-	}
-
-	board := NewBoard(size, count)
-	for board.Move() {
-	}
-}
-
+// Move will move through the game until the end
 func (b *Board) Move() bool {
 	for {
 		fmt.Println()
@@ -215,7 +192,10 @@ func (b *Board) Move() bool {
 		}
 		if b.Mines[y][x] == bomb {
 			b.Mines.show()
-			println("kaboom!")
+			fmt.Println()
+			w := (len(b.Mines) * 4) + 4
+			fmt.Println(center("K A B O O M !", w))
+			fmt.Println()
 			os.Exit(1)
 		}
 		b.Sweep(x, y)
@@ -252,4 +232,36 @@ func (b *Board) Sweep(x, y int) {
 	b.Sweep(x+1, y) // right
 	b.Sweep(x, y-1) // up
 	b.Sweep(x, y+1) // down
+}
+
+// NewBoard returns a new minesweeper game
+func NewBoard(size, count int) *Board {
+	return &Board{
+		Mines:     populate(size, count, newGrid(size)),
+		Shown:     wipe(newGrid(size)),
+		Remaining: count,
+	}
+}
+
+// center prints the string in the center per w
+func center(s string, w int) string {
+	return fmt.Sprintf("%[1]*s", -w, fmt.Sprintf("%[1]*s", (w+len(s))/2, s))
+}
+
+func main() {
+	size := 10
+	count := 5
+
+	flag.IntVar(&size, "size", size, "grid size")
+	flag.IntVar(&count, "count", count, "number of mines")
+	flag.Parse()
+
+	// make sure input was valid (was passing args w/o flags and ignoring it
+	if len(flag.Args()) > 0 {
+		fmt.Println("unknown args:", flag.Args())
+		os.Exit(1)
+	}
+
+	board := NewBoard(size, count)
+	board.Move()
 }
